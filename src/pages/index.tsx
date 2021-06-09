80cc85bc37a3fb61aec26721f92356a3790962c1
Maxat Nukhayev <maxat.nukhayev@gmail.com>
2021-06-09 11:19:48 +0200

    starter fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>Hello</div>
-  </Layout>
+  <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>Hello</div>
 )
 
 export default IndexPage
