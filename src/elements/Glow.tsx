d404833077dd68e90d95c5c501ab26215abc6ee6
Maxat Nukhayev <maxat.nukhayev@azury.de>
2021-07-15 08:57:15 +0200

    basic glow mesh with wrong blending

---
+++ src/elements/Glow.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useRef, useState } from "react"
+import * as THREE from "three"
+import { useThree, useFrame } from "@react-three/fiber"
+
+const vertexShader = `
+uniform vec3 viewVector;
+uniform float c;
+uniform float p;
+varying float intensity;
+void main() 
+{
+    vec3 vNormal = normalize( normalMatrix * normal );
+	vec3 vNormel = normalize( normalMatrix * viewVector );
+	intensity = pow( c - dot(vNormal, vNormel), p );
+	
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
+`
+const fragmentShader = `
+uniform vec3 glowColor;
+varying float intensity;
+void main() 
+{
+	vec3 glow = glowColor * intensity;
+    gl_FragColor = vec4( glow, 1.0 );
+}
+`
+
+const Glow = () => {
+  const { camera } = useThree()
+  const glowMesh = useRef<THREE.Mesh>(null)
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  const glowMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+      c: { value: 1.0 },
+      p: { value: 1.4 },
+      glowColor: { value: new THREE.Color(0xffff00) },
+      viewVector: { value: camera.position },
+    },
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    side: THREE.FrontSide,
+    blending: THREE.AdditiveBlending,
+    transparent: true,
+  })
+  const sphereGeom = new THREE.SphereGeometry(100, 32, 16)
+  const moonGlow = new THREE.Mesh(sphereGeom.clone(), glowMaterial.clone())
+  useEffect(() => {
+    const onClickListener = (event: any) => {
+      console.log("Clicked")
+      setActive(!active)
+    }
+    moonGlow.addEventListener("onClick", onClickListener)
+    console.log("Added event listener")
+    return () => {
+      moonGlow.removeEventListener("onClick", onClickListener)
+      console.log("Remove event listener")
+    }
+  }, [])
+
+  useFrame(() => {
+    if (glowMesh && glowMesh.current) {
+      moonGlow.material.uniforms.viewVector.value = new THREE.Vector3().subVectors(
+        camera.position,
+        glowMesh.current.position
+      )
+    }
+  })
+  return (
+    <primitive
+      ref={glowMesh}
+      position={[0, 0, 0]}
+      object={moonGlow}
+      scale={active ? 2 : 1}
+    />
+  )
+}
+
+export default Glow
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,44 @@
-import React from "react"
+import React, { Suspense, useState, useRef } from "react"
+import * as THREE from "three"
+import "./viewer.css"
+import { Canvas } from "@react-three/fiber"
+// import Model from "./../components/modelLoader"
+// import ThreeLoader from "../components/ThreeLoader"
+import { OrbitControls, Stage } from "@react-three/drei"
+import Glow from "../elements/Glow"
+// import Glow from "../components/glow"
 
-const IndexPage = () => (
-  <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>Hello</div>
-)
+//kadekeith.me/2017/09/12/three-glow.html
+
+const IndexPage = () => {
+  return (
+    <section className="pageContainer">
+      <>
+        <div className="canvasContainer">
+          <Canvas
+            gl={{ antialias: true }}
+            className="canvas"
+            onCreated={({ gl }) => {
+              // gl.setClearColor("lightgreen")
+            }}
+            camera={{
+              fov: 75,
+              near: 0.1,
+              far: 30000,
+              position: [0, 100, 100],
+            }}
+          >
+            <Suspense fallback={null}>
+              <Glow />
+            </Suspense>
+            {/* <ambientLight intensity={1} /> */}
+            <pointLight color={0xffffff} position={[0, 250, 0]} />
+            <OrbitControls />
+          </Canvas>
+        </div>
+      </>
+    </section>
+  )
+}
 
 export default IndexPage
